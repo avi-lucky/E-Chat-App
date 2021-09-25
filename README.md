8dff1f60f3ac2c1df150d9a49f99bd7e3a7514aa
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-26 02:46:50 +0530

    node server connection

---
+++ README.md
@@ -0,0 +1,4 @@
+# Web Chat-Application
+A Simple Web Chat Application Using Socket.IO.
+
+HTML, CSS, Node.JS, Javascript
---
+++ javascripts/client.js
@@ -0,0 +1,9 @@
+const { profile } = require('console');
+const socket = require('http://localhost:8000');
+
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.getElementById('.container')
+
+const name = prompt("Enter Your Name To Join");
+socket.emit('new-user-joined', name)
