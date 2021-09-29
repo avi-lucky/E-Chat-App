d5466bb09dae797f752c64cb5087e897b15098ce
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-30 00:12:29 +0530

    application is ready

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -11,14 +11,20 @@ const io = require('socket.io')(5000, {
 const users = {};
 
 io.on('connection', socket => {
+    // if new user joins, let other users connected to the server know!
     socket.on('new-user-joined', name => {
-        console.log("New User", name)
         users[socket.id] = name;
-        // console.log(name)
         socket.broadcast.emit('user-joined', name)
     });
 
+    // if someone send a message, broadcast it to other people
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    // if someone leaves the chat room, let others know
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
     });
 })
--- public/javascripts/client.js
+++
@@ -1,8 +0,0 @@
-// const socket = require('http://localhost:8000');
-
-const form = document.getElementById('send-container')
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.getElementById('.container')
-
-const name = prompt("Enter Your Name To Join");
-socket.emit('new-user-joined', name)
